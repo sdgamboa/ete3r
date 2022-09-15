dbbcd3d0c1ead84668bfdc3b7af5e8bfd66f4b66
Samuel D. Gamboa-Tuz <53412049+sdgamboa@users.noreply.github.com>
2022-09-15 18:34:36 -0400

    Update basilisk.R
    Add package versions.

--- R/basilisk.R
+++ R/basilisk.R
@@ -2,5 +2,5 @@ ETE3Env <- basilisk::BasiliskEnvironment(
     envname = "ETE3Env",
     pkgname = "ete3r",
     channels = "etetoolkit",
-    packages = c("ete3", "ete_toolchain")
+    packages = c("ete3==3.1.2", "ete_toolchain==3.0.0")
 )
