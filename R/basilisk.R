c8106f91e277479e00d389eb7dc3fe19edc67fe7
Samuel D. Gamboa-Tuz <53412049+sdgamboa@users.noreply.github.com>
2022-09-16 10:29:22 -0400

    Update basilisk.R

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,6 +1,6 @@
 ETE3Env <- basilisk::BasiliskEnvironment(
     envname = "ETE3Env",
     pkgname = "ete3r",
-    channels = c("conda-forge", "etetoolkit"),
+    channels = c("etetoolkit", "conda-forge"),
     packages = c("ete3==3", "ete_toolchain==3")
 )
